d5b225c51c5595169d36e33fc51dce0fe49fc79d
Lady Vargas <lady.vargas.santana@gmail.com>
2024-05-20 09:22:47 -0500

    [NEW] -> Cambios de usuario

--- resources/js/pages/vehicle/edit.vue
+++ resources/js/pages/vehicle/edit.vue
@@ -6,115 +6,191 @@
 
         <div class="card-body">
             <div class="row">
-
                 <div class="col-md-12">
-                    <form @submit.prevent="UpdateForm()" action="/vehicle" method="PUT">
+                    <form
+                        @submit.prevent="UpdateForm()"
+                        action="/vehicle"
+                        method="PUT"
+                    >
                         <div class="form-group">
                             <label for="basicInput">Marca</label>
-                            <input type="text" class="form-control " id="basicInput" placeholder="Marca"
-                                v-model="form.brand" v-bind:class="{ 'is-invalid': erorr.brand }">
+                            <input
+                                type="text"
+                                class="form-control "
+                                id="basicInput"
+                                placeholder="Marca"
+                                v-model="form.brand"
+                                v-bind:class="{ 'is-invalid': erorr.brand }"
+                            />
                             <div v-if="erorr.brand" class="invalid-feedback">
-                                {{ erorr.brand[0]  }}
+                                {{ erorr.brand[0] }}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="helpInputTop">Modelo</label>
-                            <input type="text" class="form-control" id="helpInputTop" placeholder="Modelo"
-                                v-model="form.model" v-bind:class="{ 'is-invalid': erorr.model }">
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="helpInputTop"
+                                placeholder="Modelo"
+                                v-model="form.model"
+                                v-bind:class="{ 'is-invalid': erorr.model }"
+                            />
                             <div v-if="erorr.model" class="invalid-feedback">
-                                {{ erorr.model[0]  }}
+                                {{ erorr.model[0] }}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="helpInputTop">Patente</label>
-                            <input type="text" class="form-control" id="helpInputTop" placeholder="Patente"
-                                v-model="form.license_plate" v-bind:class="{ 'is-invalid': erorr.license_plate }">
-                            <div v-if="erorr.license_plate" class="invalid-feedback">
-                                {{ erorr.license_plate[0]  }}
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="helpInputTop"
+                                placeholder="Patente"
+                                v-model="form.license_plate"
+                                v-bind:class="{
+                                    'is-invalid': erorr.license_plate
+                                }"
+                            />
+                            <div
+                                v-if="erorr.license_plate"
+                                class="invalid-feedback"
+                            >
+                                {{ erorr.license_plate[0] }}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="helpInputTop">Año</label>
-                            <input type="text" class="form-control" id="helpInputTop" placeholder="Año"
-                                v-model="form.year" v-bind:class="{ 'is-invalid': erorr.year }">
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="helpInputTop"
+                                placeholder="Año"
+                                v-model="form.year"
+                                v-bind:class="{ 'is-invalid': erorr.year }"
+                            />
                             <div v-if="erorr.year" class="invalid-feedback">
-                                {{ erorr.year[0]  }}
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="helpInputTop">Dueño</label>
-                            <input type="text" class="form-control" id="helpInputTop" placeholder="Dueño"
-                                v-model="form.id_user" v-bind:class="{ 'is-invalid': erorr.id_user }">
-                            <div v-if="erorr.id_user" class="invalid-feedback">
-                                {{ erorr.id_user[0]  }}
+                                {{ erorr.year[0] }}
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="helpInputTop">Precio</label>
-                            <input type="text" class="form-control" id="helpInputTop" placeholder="Precio"
-                                v-model="form.price" v-bind:class="{ 'is-invalid': erorr.price }">
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="helpInputTop"
+                                placeholder="Precio"
+                                v-model="form.price"
+                                v-bind:class="{ 'is-invalid': erorr.price }"
+                            />
                             <div v-if="erorr.price" class="invalid-feedback">
-                                {{ erorr.price[0]  }}
+                                {{ erorr.price[0] }}
                             </div>
                         </div>
 
-                        <router-link to="/vehicle" class="btn btn-danger">Regresar</router-link>
-                        <button type="submit" class="btn btn-success">Actualizar</button>
+                        <div class="form-group">
+                            <label for="helpInputTop">Dueño</label>
+                            <select
+                                class="form-control"
+                                v-model="form.id_user"
+                                v-bind:class="{
+                                    'is-invalid': errors && errors.id_user
+                                }"
+                            >
+                                <option value="">Seleccionar dueño</option>
+                                <option
+                                    v-for="user in users"
+                                    :key="user.id"
+                                    :value="user.id"
+                                    >{{ user.name }}</option
+                                >
+                            </select>
+                            <div
+                                v-if="errors && errors.id_user"
+                                class="invalid-feedback"
+                            >
+                                {{ errors.id_user[0] }}
+                            </div>
+                        </div>
 
+                        <router-link to="/vehicle" class="btn btn-danger"
+                            >Regresar</router-link
+                        >
+                        <button type="submit" class="btn btn-success">
+                            Actualizar
+                        </button>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
 </template>
 <script>
-    export default {
-        props : ['id'],
-        data() {
-            return {
-                form: {
-                },
-                option: [],
-                erorr: [],
-                data: {}
-            }
-        },
-        methods: {
-            getData() {
-            axios.get('/api/vehicles/' + this.id + '/edit')
+export default {
+    props: ["id"],
+    data() {
+        return {
+            form: {},
+            option: [],
+            erorr: [],
+            users: [],
+            data: {}
+        };
+    },
+    methods: {
+        getData() {
+            axios
+                .get("/api/vehicles/" + this.id + "/edit")
                 .then(response => {
                     this.form = response.data;
                 })
                 .catch(error => {
-                    console.error('Error fetching vehicle data:', error);
+                    console.error("Error fetching vehicle data:", error);
                 });
         },
-            UpdateForm() {
-                this.form.created_at = new Date(this.form.created_at).toISOString().slice(0, 19).replace('T', ' ');
-                this.form.updated_at = new Date(this.form.updated_at).toISOString().slice(0, 19).replace('T', ' ');
-                axios.put('/api/vehicles/' + this.id, this.form).then((response) => {
+        getDataUser() {
+            axios
+                .get("/api/users")
+                .then(response => {
+                    this.users = response.data;
+                })
+                .catch(error => {
+                    console.error("Error fetching user data:", error);
+                });
+        },
+        UpdateForm() {
+            this.form.created_at = new Date(this.form.created_at)
+                .toISOString()
+                .slice(0, 19)
+                .replace("T", " ");
+            this.form.updated_at = new Date(this.form.updated_at)
+                .toISOString()
+                .slice(0, 19)
+                .replace("T", " ");
+            axios
+                .put("/api/vehicles/" + this.id, this.form)
+                .then(response => {
                     if (response.data.status) {
                         // console.log(response.data.messege);
                         this.$noty.success(response.data.messege);
                         this.$router.push({
-                            name: 'vehicle',
+                            name: "vehicle"
                         });
                     }
-                }).catch((erorr) => {
-                    // console.log(erorr.response.data.errors);
-                    this.erorr = erorr.response.data.errors
                 })
-            }
-        },
-        mounted() {
-            this.getData();
-        },
+                .catch(erorr => {
+                    // console.log(erorr.response.data.errors);
+                    this.erorr = erorr.response.data.errors;
+                });
+        }
+    },
+    mounted() {
+        this.getData();
+        this.getDataUser();
     }
-
+};
 </script>
--- resources/js/pages/vehicle/index.vue
+++ resources/js/pages/vehicle/index.vue
@@ -7,14 +7,25 @@
                     <v-toolbar-title>Lista de vehículos</v-toolbar-title>
                     <v-divider class="mx-4" inset vertical></v-divider>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" @click.prevent="createRoute()" dark class="mb-2">
+                    <v-btn
+                        color="primary"
+                        @click.prevent="createRoute()"
+                        dark
+                        class="mb-2"
+                    >
                         Crear
                     </v-btn>
                 </v-toolbar>
             </template>
             <v-card-title>
                 <v-spacer></v-spacer>
-                <v-text-field v-model="search" append-icon="mdi-magnify" label="Buscar" single-line hide-details>
+                <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Buscar"
+                    single-line
+                    hide-details
+                >
                 </v-text-field>
             </v-card-title>
             <v-data-table
@@ -31,7 +42,7 @@
                         <td>{{ item.model }}</td>
                         <td>{{ item.license_plate }}</td>
                         <td>{{ item.year }}</td>
-                        <td>{{ item.id_user }}</td>
+                        <td>{{ userNameMap[item.id_user] }}</td>
                         <td>{{ item.price }}</td>
                         <td>
                             <v-icon @click="editRoute(item)">mdi-pencil</v-icon>
@@ -43,99 +54,113 @@
                 </template>
             </v-data-table>
         </div>
-
     </div>
-
 </template>
 
-
 <script>
-    export default {
-        data() {
-            return {
-                headers: [{
-                        text: 'id', // nama header
-                        value: 'id' //  value dari database
-                    },
-                    {
-                        text: 'Marca',
-                        value: 'brand'
-                    },
-                    {
-                        text: 'Modelo',
-                        value: 'model', // action dari v-slot:item.action
-                    },
-                    {
-                        text: 'Patente',
-                        value: 'license_plate'
-                    },
-                    {
-                        text: 'Año',
-                        value: 'year'
-                    },
-                    {
-                        text: 'Dueño',
-                        value: 'id_user'
-                    },
-                    {
-                        text: 'Precio',
-                        value: 'price'
-                    },
-                ],
-                data: [], // isi data yang ada du databse
-                search: '',
-                modelDelete: false
-            }
-        },
-        methods: {
-            GetData() {
-                axios.get('/api/vehicles').then((response) => {
-                    this.data = response.data
-                }).catch((eror) => {
+export default {
+    data() {
+        return {
+            headers: [
+                {
+                    text: "id", // nama header
+                    value: "id" //  value dari database
+                },
+                {
+                    text: "Marca",
+                    value: "brand"
+                },
+                {
+                    text: "Modelo",
+                    value: "model" // action dari v-slot:item.action
+                },
+                {
+                    text: "Patente",
+                    value: "license_plate"
+                },
+                {
+                    text: "Año",
+                    value: "year"
+                },
+                {
+                    text: "Dueño",
+                    value: "id_user"
+                },
+                {
+                    text: "Precio",
+                    value: "price"
+                }
+            ],
+            data: [], // isi data yang ada du databse
+            search: "",
+            modelDelete: false,
+            userNameMap: {},
+        };
+    },
+    methods: {
+        GetData() {
+            axios
+                .get("/api/vehicles")
+                .then(response => {
+                    this.data = response.data;
+                })
+                .catch(eror => {
                     console.log(eror);
-                })
-            },
-            createRoute() {
-                this.$router.push('/vehicle/create')
-            },
-            editRoute(item) {
-            this.$router.push({ name: 'edit_vehicle', params: { id: item.id } });
-            },
-            deleteItem(item) {
-                console.log(item.id);
-                Swal.fire({
-                    title: "¿Está seguro que desea eliminar?",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Si',
-                    cancelButtonText: 'No'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        axios.delete('/api/vehicles/' + item.id).then((response) => {
-                            const idx = this.data.indexOf(item)
-                            this.data.splice(idx, 1)
-                            // alert
-                            Swal.fire(
-                                'Eliminado!',
-                                'Se ha eliminado correctamente.',
-                                'success'
-                            )
-                        })
-
-                    }
-                })
-
-            },
-            editItem(item) {
-                this.$router.push('/vehicle/' + item.id);
+                });
+        },
+        async loadUserNames() {
+            try {
+                const response = await axios.get("/api/users");
+                this.userNameMap = response.data.reduce((acc, user) => {
+                    acc[user.id] = user.name;
+                    return acc;
+                }, {});
+            } catch (error) {
+                console.error("Error al cargar los nombres de usuario:", error);
             }
         },
-
-        mounted() {
-            this.GetData()
+        createRoute() {
+            this.$router.push("/vehicle/create");
         },
-    }
+        editRoute(item) {
+            this.$router.push({
+                name: "edit_vehicle",
+                params: { id: item.id }
+            });
+        },
+        deleteItem(item) {
+            console.log(item.id);
+            Swal.fire({
+                title: "¿Está seguro que desea eliminar?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Si",
+                cancelButtonText: "No"
+            }).then(result => {
+                if (result.isConfirmed) {
+                    axios.delete("/api/vehicles/" + item.id).then(response => {
+                        const idx = this.data.indexOf(item);
+                        this.data.splice(idx, 1);
+                        // alert
+                        Swal.fire(
+                            "Eliminado!",
+                            "Se ha eliminado correctamente.",
+                            "success"
+                        );
+                    });
+                }
+            });
+        },
+        editItem(item) {
+            this.$router.push("/vehicle/" + item.id);
+        }
+    },
 
+    mounted() {
+        this.GetData();
+        this.loadUserNames();
+    }
+};
 </script>
